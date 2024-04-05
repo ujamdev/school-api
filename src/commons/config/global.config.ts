2b8fb9e4337cd90995fc73c6d977cb58ba470c77
ujamdev <ujamdev@gmail.com>
2024-04-05 18:14:02 +0900

    feat: global.config 구현

---
+++ src/commons/config/global.config.ts
@@ -0,0 +1,22 @@
+import * as yaml from 'js-yaml';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import * as process from 'process';
+
+const LOCAL_CONFIG = 'local';
+const EXTENSION = '.yml';
+
+const CURRENT_WORKING_PATH = process.cwd();
+const CONFIG_DIR = CURRENT_WORKING_PATH + '/' + 'config';
+const CHARACTER_SET = 'utf8';
+
+export default () => {
+  return yaml.load(readConfigFile()) as Record<string, any>;
+};
+
+function readConfigFile() {
+  const fileName = LOCAL_CONFIG;
+  const filePath = fileName + EXTENSION;
+
+  return readFileSync(join(CONFIG_DIR, filePath), CHARACTER_SET);
+}
