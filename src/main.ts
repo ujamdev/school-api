c4888151fe546ddf578f7fdd6e8ee8e951a2fc47
ujamdev <ujamdev@gmail.com>
2024-04-05 18:15:02 +0900

    feat: 서버 클래스 생성 애플리케이션 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+class Server {
+  private readonly configService: ConfigService;
+  private readonly port;
+
+  constructor(private readonly application: NestExpressApplication) {
+    this.configService = application.get(ConfigService);
+    this.port = this.configService.get('server.port') || 3000;
+  }
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
+
 bootstrap();
