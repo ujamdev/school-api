1c089b4897aed55db313d999de5eecfce54d4fe1
ujamdev <ujamdev@gmail.com>
2024-04-07 01:03:41 +0900

    feat: 소식 생성 핸들러 구현

--- src/domains/school/presentation/school.controller.ts
+++ src/domains/school/presentation/school.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { MessageResponse } from 'src/commons/dto/message.response';
 import { SchoolService } from '../application/school.service';
-import { createSchoolRequest } from '../domain/dto/create.school.request';
+import { CreateNotificationRequest } from '../domain/dto/create.notification.request';
+import { CreateSchoolRequest } from '../domain/dto/create.school.request';
 
 @Controller('api')
 export class SchoolController {
@@ -10,10 +11,20 @@ export class SchoolController {
   /**
    * 학교 관리자가 지역, 학교 명으로 학교 생성
    * @param {CreatePushRequest} request
-   * @return {Promise<InsertResult>}
+   * @return {Promise<MessageResponse>}
    */
   @Post('/schools')
-  async createSchool(@Body() request: createSchoolRequest): Promise<MessageResponse> {
+  async createSchool(@Body() request: CreateSchoolRequest): Promise<MessageResponse> {
     return await this.schoolService.createSchool(request);
+  }
+
+  /**
+   * 학교 관리자는 학교 페이지 내에 소식 생성
+   * @param {createNotificationRequest} request
+   * @return {Promise<MessageResponse>}
+   */
+  @Post('/notifications')
+  async createNotification(@Body() request: CreateNotificationRequest): Promise<MessageResponse> {
+    return await this.schoolService.createNotification(request);
   }
 }
