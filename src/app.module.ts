af854a25dcf8497f8b0514310dd0284430198600
ujamdev <ujamdev@gmail.com>
2024-04-06 14:00:06 +0900

    chore: 앱 모듈에 db 관련 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import globalConfig from './commons/config/global.config';
-import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './commons/typeorm/database.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ load: [globalConfig], isGlobal: true }),
-  ],
+  imports: [ConfigModule.forRoot({ load: [globalConfig], isGlobal: true }), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
