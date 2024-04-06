1fcd3d6004f26bdc8e8cd709fd0afb20954647e1
ujamdev <ujamdev@gmail.com>
2024-04-07 01:03:32 +0900

    feat: 소식 생성 로직 구현

--- src/domains/school/application/school.service.ts
+++ src/domains/school/application/school.service.ts
@@ -1,13 +1,18 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { MessageResponse } from 'src/commons/dto/message.response';
-import { createSchoolRequest } from '../domain/dto/create.school.request';
-import { schoolRepository } from '../domain/school.repository';
+import { CreateNotificationRequest } from '../domain/dto/create.notification.request';
+import { CreateSchoolRequest } from '../domain/dto/create.school.request';
+import { NotificationRepository } from '../domain/notification.repository';
+import { SchoolRepository } from '../domain/school.repository';
 
 @Injectable()
 export class SchoolService {
-  constructor(private readonly schoolRepository: schoolRepository) { }
+  constructor(
+    private readonly schoolRepository: SchoolRepository,
+    private readonly notificationRepository: NotificationRepository,
+  ) { }
 
-  async createSchool(request: createSchoolRequest): Promise<MessageResponse> {
+  async createSchool(request: CreateSchoolRequest): Promise<MessageResponse> {
     try {
       const result = await this.schoolRepository.createSchool(request);
 
@@ -18,6 +23,20 @@ export class SchoolService {
       return MessageResponse.of('학교 등록에 성공했습니다.');
     } catch (error) {
       throw new Error(`Failed to create school: ${error}`);
+    }
+  }
+
+  async createNotification(request: CreateNotificationRequest): Promise<MessageResponse> {
+    try {
+      const result = await this.notificationRepository.createNotification(request);
+
+      if (result.raw.affectedRows === 0) {
+        throw new BadRequestException(`Notification was not successfully created`);
+      }
+
+      return MessageResponse.of('소식 등록에 성공했습니다.');
+    } catch (error) {
+      throw new Error(`Failed to create notification: ${error}`);
     }
   }
 }
