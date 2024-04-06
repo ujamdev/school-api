fffa7e61c249beb78bdabd5a47ba2b69a289f199
ujamdev <ujamdev@gmail.com>
2024-04-06 17:13:12 +0900

    chore: 학생 모듈 설정
    student.module.ts
     - 학생 엔티티, 학생-학교 엔티티 설정

---
+++ src/domains/student/student.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { studentEntity, studentEntity } from './domain/student.entity';
+import { studentSchoolEntity } from './domain/student.school.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([studentEntity, studentSchoolEntity])],
+})
+export class StudentModule { }
