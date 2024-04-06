e0ebf811fc72933a61acad759c6150276ae72549
ujamdev <ujamdev@gmail.com>
2024-04-07 00:24:48 +0900

    feat: 학교 생성 로직, 핸들러 반환 타입 추가

--- src/domains/school/presentation/school.controller.ts
+++ src/domains/school/presentation/school.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { MessageResponse } from 'src/commons/dto/message.response';
 import { SchoolService } from '../application/school.service';
 import { createSchoolRequest } from '../domain/dto/create.school.request';
 
@@ -12,7 +13,7 @@ export class SchoolController {
    * @return {Promise<InsertResult>}
    */
   @Post('/schools')
-  async createSchool(@Body() request: createSchoolRequest) {
+  async createSchool(@Body() request: createSchoolRequest): Promise<MessageResponse> {
     return await this.schoolService.createSchool(request);
   }
 }
