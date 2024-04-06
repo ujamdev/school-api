4c2463df980835aef50b0c8714f2db602e4c6718
ujamdev <ujamdev@gmail.com>
2024-04-06 21:23:28 +0900

    feat: 학교 생성 핸들러 구현

---
+++ src/domains/school/presentation/school.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { SchoolService } from '../application/school.service';
+import { createSchoolRequest } from '../domain/dto/create.school.request';
+
+@Controller('api')
+export class SchoolController {
+  constructor(private readonly schoolService: SchoolService) { }
+
+  /**
+   * 학교 관리자가 지역, 학교 명으로 학교 생성
+   * @param {CreatePushRequest} request
+   * @return {Promise<InsertResult>}
+   */
+  @Post('/schools')
+  async createSchool(@Body() request: createSchoolRequest) {
+    return await this.schoolService.createSchool(request);
+  }
+}
