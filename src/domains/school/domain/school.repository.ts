1c24f742911422dd33aff1c5c50ead8e7f40f208
ujamdev <ujamdev@gmail.com>
2024-04-07 14:00:45 +0900

    feat: 구독중인 학교 조회 쿼리 구현

--- src/domains/school/domain/school.repository.ts
+++ src/domains/school/domain/school.repository.ts
@@ -1,3 +1,5 @@
+import { YesNo } from 'src/commons/enum/yes.no';
+import { GetStudentSchoolsRequest } from 'src/domains/student/domain/dto/get.student.schools.request';
 import { InsertResult, Repository } from 'typeorm';
 import { CustomRepository } from '../../../commons/decorator/typeorm.decorator';
 import { CreateSchoolRequest } from './dto/create.school.request';
@@ -11,5 +13,22 @@ export class SchoolRepository extends Repository<SchoolEntity> {
       .into(SchoolEntity)
       .values({ regionId: request.regionId, name: request.name })
       .execute();
+  }
+
+  async findSubscribeSchools(
+    studentId: number,
+    request: GetStudentSchoolsRequest,
+  ): Promise<SchoolEntity[]> {
+    const { page, perPage } = request;
+
+    return await this.createQueryBuilder('school')
+      .select([`school.id`, `school.name`, `studentSchool.studentId`, `studentSchool.createdAt`])
+      .leftJoin('school.studentSchool', 'studentSchool')
+      .where('studentSchool.student_id = :studentId', { studentId })
+      .andWhere('studentSchool.is_active = :isActive', { isActive: YesNo.YES })
+      .take(perPage)
+      .skip(perPage * (page - 1))
+      .orderBy('studentSchool.createdAt', 'DESC')
+      .getMany();
   }
 }
