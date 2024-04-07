2fe52856852eae8d0ea617b863c494863be8bbda
ujamdev <ujamdev@gmail.com>
2024-04-07 13:54:25 +0900

    feat: 구독중인 학교 조회 요청 파라미터 추가
    src/main.ts
     - 타입 변환 관련하여 ValidationPipe 설정

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SuccessHandler } from './commons/handler/success.handler';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new SuccessHandler());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   await app.listen(8080);
 }
