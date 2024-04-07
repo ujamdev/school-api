eddc21c15c7c47b72d6583d18c129308dfee0b40
ujamdev <ujamdev@gmail.com>
2024-04-07 12:11:27 +0900

    feat: 학생이 학교 구독 업데이트 로직 구현

--- src/domains/student/application/student.service.ts
+++ src/domains/student/application/student.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { MessageResponse } from 'src/commons/dto/message.response';
 import { StudentSchoolEntity } from '../domain/student.school.entity';
 import { StudentSchoolRepository } from '../domain/student.school.repository';
 
@@ -8,5 +9,22 @@ export class StudentService {
 
   async getStudentSchool(studentId: number, schoolId: number): Promise<StudentSchoolEntity> {
     return await this.studentSchoolRepository.getStudentSchool(studentId, schoolId);
+  }
+
+  async updateSubscribeToSchool(studentId: number, schoolId: number): Promise<MessageResponse> {
+    try {
+      const result = await this.studentSchoolRepository.updateSubscribeToSchool(
+        studentId,
+        schoolId,
+      );
+
+      if (result.affected === 0) {
+        throw new BadRequestException(`Subscribe to school was not successfully updated`);
+      }
+
+      return MessageResponse.of('학생이 학교 구독에 성공했습니다.');
+    } catch (error) {
+      throw new Error(`Failed to update Subscribe to school: ${error}`);
+    }
   }
 }
