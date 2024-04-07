da69b8a45e65355ecb44305c1be4daf2c971d850
ujamdev <ujamdev@gmail.com>
2024-04-07 11:16:18 +0900

    feat: 학생이 학교 구독 쿼리 구현
    student.module.ts
     - 학생 모듈에 StudentSchoolRepository 커스텀 리포지토리 등록

--- src/domains/student/student.module.ts
+++ src/domains/student/student.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomTypeormModule } from 'src/commons/decorator/custom.typeorm.module';
+import { StudentService } from './application/student.service';
 import { StudentEntity } from './domain/student.entity';
 import { StudentSchoolEntity } from './domain/student.school.entity';
+import { StudentSchoolRepository } from './domain/student.school.repository';
+import { StudentController } from './presentation/student.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StudentEntity, StudentSchoolEntity])],
+  imports: [
+    TypeOrmModule.forFeature([StudentEntity, StudentSchoolEntity]),
+    CustomTypeormModule.forCustomRepository([StudentSchoolRepository]),
+  ],
+  providers: [StudentService],
+  controllers: [StudentController],
 })
 export class StudentModule { }
