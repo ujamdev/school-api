54fec2245bb484ab15b9d5eedd9aa7117c076323
ujamdev <ujamdev@gmail.com>
2024-04-07 11:15:29 +0900

    feat: app 모듈에 student 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AppService } from './app.service';
 import globalConfig from './commons/config/global.config';
 import { DatabaseModule } from './commons/typeorm/database.module';
 import { SchoolModule } from './domains/school/school.module';
+import { StudentModule } from './domains/student/student.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [globalConfig], isGlobal: true }),
     DatabaseModule,
     SchoolModule,
+    StudentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
