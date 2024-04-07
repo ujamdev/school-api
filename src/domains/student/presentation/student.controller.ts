8ff51608863f99fa05bab923ca8ff183305c7ad7
ujamdev <ujamdev@gmail.com>
2024-04-07 12:37:41 +0900

    feat: 학생이 학교 구독 취소 핸들러 구현

--- src/domains/student/presentation/student.controller.ts
+++ src/domains/student/presentation/student.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Param, Post } from '@nestjs/common';
+import { Controller, Delete, Param, Post } from '@nestjs/common';
 import { MessageResponse } from 'src/commons/dto/message.response';
 import { StudentService } from '../application/student.service';
 import { CreateStudentSchoolRequest } from '../domain/dto/create.student.school.request';
+import { DeleteStudentSchoolRequest } from '../domain/dto/delete.student.school.request';
 
 @Controller('api')
 export class StudentController {
@@ -15,5 +16,15 @@ export class StudentController {
   @Post('/students/:studentId/schools/:schoolId')
   async createStudentSchool(@Param() param: CreateStudentSchoolRequest): Promise<MessageResponse> {
     return await this.studentService.createStudentSchool(param);
+  }
+
+  /**
+   * 학생이 구독중인 학교 구독 취소
+   * @param {DeleteStudentSchoolRequest} param
+   * @return {Promise<MessageResponse>}
+   */
+  @Delete('/students/:studentId/schools/:schoolId')
+  async deleteStudentSchool(@Param() param: DeleteStudentSchoolRequest): Promise<MessageResponse> {
+    return await this.studentService.deleteStudentSchool(param);
   }
 }
