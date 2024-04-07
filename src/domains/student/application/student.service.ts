1c67bce8306b5b6d1deba1f5f944bab157297ff7
ujamdev <ujamdev@gmail.com>
2024-04-07 11:55:53 +0900

    feat: 학생-학교 조회 로직 구현

--- src/domains/student/application/student.service.ts
+++ src/domains/student/application/student.service.ts
@@ -1,23 +1,12 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { MessageResponse } from 'src/commons/dto/message.response';
-import { CreateStudentSchoolRequest } from '../domain/dto/create.student.school.request';
+import { Injectable } from '@nestjs/common';
+import { StudentSchoolEntity } from '../domain/student.school.entity';
 import { StudentSchoolRepository } from '../domain/student.school.repository';
 
 @Injectable()
 export class StudentService {
   constructor(private readonly studentSchoolRepository: StudentSchoolRepository) { }
 
-  async createStudentSchool(request: CreateStudentSchoolRequest): Promise<MessageResponse> {
-    try {
-      const result = await this.studentSchoolRepository.createStudentSchool(request);
-
-      if (result.raw.affectedRows === 0) {
-        throw new BadRequestException(`School subscribe was not successfully created`);
-      }
-
-      return MessageResponse.of('학생이 학교 구독에 성공했습니다.');
-    } catch (error) {
-      throw new Error(`Failed to create school subscribe: ${error}`);
-    }
+  async getStudentSchool(studentId: number, schoolId: number): Promise<StudentSchoolEntity> {
+    return await this.studentSchoolRepository.getStudentSchool(studentId, schoolId);
   }
 }
