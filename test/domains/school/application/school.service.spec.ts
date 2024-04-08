1e4053b721ff74460bdad28a2be1a822db2e6948
ujamdev <ujamdev@gmail.com>
2024-04-08 11:43:33 +0900

    test: 소식 등록 테스트 구현

--- test/domains/school/application/school.service.spec.ts
+++ test/domains/school/application/school.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { InsertResult, UpdateResult } from 'typeorm';
 import { MessageResponse } from '../../../../src/commons/dto/message.response';
 import { SchoolService } from '../../../../src/domains/school/application/school.service';
+import { CreateNotificationRequest } from '../../../../src/domains/school/domain/dto/create.notification.request';
 import { CreateSchoolRequest } from '../../../../src/domains/school/domain/dto/create.school.request';
 import { NotificationRepository } from '../../../../src/domains/school/domain/notification.repository';
 import { SchoolRepository } from '../../../../src/domains/school/domain/school.repository';
@@ -35,6 +36,27 @@ describe('SchoolService', () => {
 
       // then
       expect(result).toEqual(MessageResponse.of('학교 등록에 성공했습니다.'));
+    });
+  });
+
+  describe('createNotification', () => {
+    it('should successfully create a notification', async () => {
+      // given
+      const request = {
+        schoolId: 1,
+        content: '서울초등학교 소식입니다',
+        registerId: 1,
+      } as CreateNotificationRequest;
+
+      jest
+        .spyOn(notificationRepository, 'createNotification')
+        .mockResolvedValue({ raw: { affectedRows: 1 } } as InsertResult);
+
+      // when
+      const result = await service.createNotification(request);
+
+      // then
+      expect(result).toEqual(MessageResponse.of('소식 등록에 성공했습니다.'));
     });
   });
 
