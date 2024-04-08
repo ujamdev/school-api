4d944ebba5e393043a432aec47f64e0d480e65a3
ujamdev <ujamdev@gmail.com>
2024-04-08 22:17:05 +0900

    chore: 전역 예외 필터 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './commons/filter/http.exception.filter';
 import { SuccessHandler } from './commons/handler/success.handler';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new SuccessHandler());
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
