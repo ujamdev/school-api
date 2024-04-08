a3dedda5d1ee967868bfe082a0cc5cc8a2ebfa03
ujamdev <ujamdev@gmail.com>
2024-04-08 13:14:24 +0900

    test: 소식 수정 테스트 구현

--- test/domains/school/application/school.service.spec.ts
+++ test/domains/school/application/school.service.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InsertResult, UpdateResult } from 'typeorm';
 import { MessageResponse } from '../../../../src/commons/dto/message.response';
+import { YesNo } from '../../../../src/commons/enum/yes.no';
 import { SchoolService } from '../../../../src/domains/school/application/school.service';
 import { CreateNotificationRequest } from '../../../../src/domains/school/domain/dto/create.notification.request';
 import { CreateSchoolRequest } from '../../../../src/domains/school/domain/dto/create.school.request';
+import { GetNotificationResponse } from '../../../../src/domains/school/domain/dto/get.notification.response';
+import { UpdateNotificationRequest } from '../../../../src/domains/school/domain/dto/update.notification.request';
 import { NotificationRepository } from '../../../../src/domains/school/domain/notification.repository';
 import { SchoolRepository } from '../../../../src/domains/school/domain/school.repository';
 
@@ -42,11 +45,11 @@ describe('SchoolService', () => {
   describe('createNotification', () => {
     it('should successfully create a notification', async () => {
       // given
-      const request = {
+      const request: CreateNotificationRequest = {
         schoolId: 1,
         content: '서울초등학교 소식입니다',
         registerId: 1,
-      } as CreateNotificationRequest;
+      };
 
       jest
         .spyOn(notificationRepository, 'createNotification')
@@ -57,6 +60,39 @@ describe('SchoolService', () => {
 
       // then
       expect(result).toEqual(MessageResponse.of('소식 등록에 성공했습니다.'));
+    });
+  });
+
+  describe('updateNotification', () => {
+    it('should update notification successfully', async () => {
+      // given
+      const notificationId = 1;
+      const updateRequest: UpdateNotificationRequest = {
+        content: '서울초등학교 소식2입니다',
+        modifierId: 1,
+      };
+
+      const mockNotification: GetNotificationResponse = {
+        id: 1,
+        schoolId: 1,
+        content: '서울초등학교 소식입니다',
+        registerId: 1,
+        modifierId: null,
+        isActive: YesNo.YES,
+        createdAt: new Date('2024-04-06T16:09:59.000Z'),
+        updatedAt: new Date('2024-04-06T16:09:59.000Z'),
+      };
+
+      // when
+      jest.spyOn(notificationRepository, 'getNotification').mockResolvedValue(mockNotification);
+      jest
+        .spyOn(notificationRepository, 'updateNotification')
+        .mockResolvedValue({ affected: 1 } as UpdateResult);
+
+      const result = await service.updateNotification(notificationId, updateRequest);
+
+      // then
+      expect(result).toEqual(expect.objectContaining({ message: '소식 수정에 성공했습니다.' }));
     });
   });
 
